55b9973a6b09fd4863994bbb33c8aeee6b7dcd49
TEAMSTD09 <110489709+TEAMSTD09@users.noreply.github.com>
2022-08-20 00:29:18 +0530

    Update GitGram.py

--- GitGram.py
+++ GitGram.py
@@ -31,7 +31,7 @@ else:
 updater = Updater(token=BOT_TOKEN, workers=1)
 dispatcher = updater.dispatcher
 
-print("If you need more help from Me, Join @DevilsHeavenMF in Telegram.")
+print("If you need more help from Me, Join @teamstd09 in Telegram.")
 
 
 def start(_bot, update):
