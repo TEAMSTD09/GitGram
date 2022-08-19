aafec34f3b85c59f86f8aedd68020827ce94980e
TEAMSTD09 <110489709+TEAMSTD09@users.noreply.github.com>
2022-08-20 00:04:03 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Your Bot token in the quotes taken from @BotFather on Telegram
+BOT_TOKEN = "5020504303:AAElG7ieBhOGHDJOgO3Ky0qq8ssTRAeWDTo"
+
+# Project Name of your Project
+PROJECT_NAME = "GitGram"
+
+# If you forked the Repository, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/TEAMSTD09/GitGram"
